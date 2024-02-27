bb7b6d14e7a62e0c85a86481ca40d969ddada18c
João Serafim <joaopixi48050e@gmail.com>
2024-02-27 19:09:35 -0300

    operadores de incremento e decremento - aula 9 https://youtu.be/4Qq0FNY9lOw?si=Jgw1CTpcn_Br8ACk

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
 import java.util.Scanner; // Importa a classe Scanner do pacote java.util, que é usada para receber entrada do usuário.
 
 public class Main { // Define uma classe chamada Main.
-    public static void main(String[] args) { // Define o método principal da classe Main.
-        double total = 0; // Declara uma variável double chamada total e a inicializa com 0.
-        Scanner scanner = new Scanner(System.in);  // Cria um objeto Scanner chamado scanner associado à entrada do sistema.
+    public static void main(String[] args) { // Define o método principal da classe Main
+        int fila = 0;
+        //incremento e decremento
+        fila +=1; // o mesmo que +1
+        fila +=1; // o mesmo que +1
+        fila ++; // o mesmo que +1
+        fila ++; // o mesmo que +1
+        fila --; // o mesmo que -1
 
-        System.out.println("=========================="); // Imprime uma linha de separação.
-        System.out.println("==== Caixa supermercado ===="); // Imprime uma mensagem indicando o início do programa.
 
-        System.out.print("Insira o valor do primeiro produto: "); // Imprime uma mensagem solicitando ao usuário que insira o valor do primeiro produto.
-        total += scanner.nextDouble(); // Lê um valor double digitado pelo usuário e adiciona ao total.
+        System.out.println("Na fila há " +fila+ " pessoas");
 
-        System.out.print("Insira o valor do segundo produto: "); // Imprime uma mensagem solicitando ao usuário que insira o valor do segundo produto.
-        total += scanner.nextDouble(); // Lê um valor double digitado pelo usuário e adiciona ao total.
+        fila = 0;
+        System.out.println(fila++); /*aqui será imprimido o valor  0 da variável fila, pois o operador
+        de decremento está após o nome da variável, o ++ deveria vir antes para ser imprimido, se eu fizesse (++fila) iria imprimir o valor 1*/
+        System.out.println(fila); //o anterior adcionou um, aqui mantém o 1
+        System.out.println(++fila); // o anterior manteve o 1, aqui adciona +1 = 2
+        System.out.println(--fila); // o anterior teve como resultado 2, aqui subtrai 1 = 1
+        System.out.println(fila); // o anterior foi igual a 1, aqui matém o 1, sem incremento ou decremento.
 
-        System.out.println("O total da compra é: " + total); // Imprime o total da compra.
     }
 }
